9a36605a9344aa3a8f609da5cfc6f41a022e6a16
Drew <84882983+vdr3w@users.noreply.github.com>
2023-08-30 15:11:50 -0300

    feat(StudentManagement): add initial structure

---
+++ projeto-modulo-01/src/views/StudentManagement/StudentManagement.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="header">
+      <h1>ALUNOS</h1>
+      <button class="new-button">NOVO</button>
+    </div>
+    <div class="search-bar">
+      <input type="text" placeholder="Buscar aluno" v-model="searchQuery" />
+      <button class="search-button">Buscar</button>
+    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>NOME</th>
+          <th>AÇÕES</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- Placeholder para os alunos -->
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup>
+// Placeholder para a lógica do componente
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.search-bar {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+}
+</style>
