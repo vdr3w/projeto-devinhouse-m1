2aab69d5fc2a50087e8cb1602057fc1398b36f33
Drew <84882983+vdr3w@users.noreply.github.com>
2023-08-30 14:49:49 -0300

    chore(router): add dashboard route

--- projeto-modulo-01/src/router/index.js
+++ projeto-modulo-01/src/router/index.js
@@ -1,21 +1,29 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "../views/Login/Login.vue";
 import RegisterUser from "../views/Register/RegisterUser.vue";
+import Dashboard from "../views/Dashboard/Dashboard.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: RegisterUser,
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    component: Dashboard,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "login",
-      component: Login,
-    },
-    {
-      path: "/register",
-      name: "registerUser",
-      component: RegisterUser,
-    },
-  ],
+  routes,
 });
 
 export default router;
