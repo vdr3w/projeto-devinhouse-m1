9eac8785a13820bb41a28b32f549fd7db0b498f8
Drew <84882983+vdr3w@users.noreply.github.com>
2023-09-03 11:57:28 -0300

    feat(login): Implement validation and authentication

--- projeto-modulo-01/src/views/Login/Login.vue
+++ projeto-modulo-01/src/views/Login/Login.vue
@@ -1,50 +1,90 @@
 <template>
-  <div>
-    <form @submit.prevent="submitLogin" class="login-form">
-      <div class="error-container">
-        <ul>
-          <li v-if="emailError">{{ emailError }}</li>
-          <li v-if="passwordError">{{ passwordError }}</li>
-        </ul>
-      </div>
+  <v-container class="fill-height">
+    <v-row justify="center" align="center">
+      <v-card
+        align="center"
+        min-width="300"
+        max-width="500"
+        class="elevation-12">
+        <v-card-title><v-icon icon="$vuetify"></v-icon> </v-card-title>
+        <v-form ref="form" @submit.prevent="handleSubmit" class="login-form">
+          <v-snackbar v-if="emailError || passwordError">
+            {{ emailError }} {{ passwordError }}
+          </v-snackbar>
 
-      <input placeholder="Enter Email" v-model="userEmail" />
-      <input
-        type="password"
-        placeholder="Enter Password"
-        v-model="userPassword" />
+          <v-text-field
+            label="Digite seu e-mail"
+            v-model="usuario.email"
+            :rules="[(value) => !!value || 'O e-mail é obrigatório!']"
+            required></v-text-field>
 
-      <button class="submit-button" type="submit">Log In</button>
+          <v-text-field
+            label="Digite sua senha"
+            type="password"
+            v-model="usuario.password"
+            :rules="[(value) => !!value || 'A senha é obrigatória!']"
+            required></v-text-field>
 
-      <p>
-        <router-link to="/register">
-          <button class="register-button">Sign Up</button>
-        </router-link>
-      </p>
-    </form>
-  </div>
+          <v-btn type="submit">Entrar</v-btn>
+
+          <p>
+            <router-link to="/register">
+              <v-btn prepend-icon="mdi-account-plus"> Novo Usuário </v-btn>
+            </router-link>
+          </p>
+        </v-form>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
-      userEmail: "",
-      userPassword: "",
-
+      usuario: {
+        email: "",
+        password: "",
+      },
       emailError: "",
       passwordError: "",
     };
   },
   methods: {
-    submitLogin() {
-      this.emailError = "";
-      this.passwordError = "";
-      if (this.userEmail === "") this.emailError = "Enter Email";
-      if (this.userPassword === "") this.passwordError = "Enter Password";
+    async handleSubmit() {
+      const { valid } = await this.$refs.form.validate();
+
+      if (!valid) {
+        this.emailError = "O e-mail é obrigatório!";
+        this.passwordError = "A senha é obrigatória!";
+        return;
+      }
+
+      try {
+        const result = await axios.post(
+          "http://localhost:3000/sessions",
+          this.usuario
+        );
+
+        if (result.status == 200) {
+          localStorage.setItem("user-info", JSON.stringify(result.data));
+          this.$router.push("/");
+        }
+      } catch (error) {
+        console.log(error.response.data.error);
+        this.emailError = "Usuário não cadastrado!";
+        this.passwordError = "";
+        alert("Usuário não cadastrado!");
+      }
     },
   },
 };
 </script>
 
-<style scoped></style>
+<style>
+.v-snackbar {
+  margin-top: 20px;
+}
+</style>
