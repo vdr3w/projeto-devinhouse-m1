2911830e6e1df9f269d49f2f56332f562f78b856
Drew <84882983+vdr3w@users.noreply.github.com>
2023-09-05 03:10:17 -0300

    feat(App.vue): implement Vuetify components and custom color scheme

--- projeto-modulo-01/src/App.vue
+++ projeto-modulo-01/src/App.vue
@@ -1,14 +1,74 @@
 <template>
-  <div>
-    <header>
-      <nav>
-        <router-link to="/">Home</router-link>
-        <router-link to="/alunos">Alunos</router-link>
-        <router-link to="/exercicios">Exercícios</router-link>
-        <router-link to="/login">Sair</router-link>
-      </nav>
-    </header>
-    <RouterView />
+  <div style="background-color: var(--lavender-blush)">
+    <v-app style="background-color: var(--lavender-blush)">
+      <v-navigation-drawer
+        app
+        style="
+          background-color: var(--licorice);
+          color: var(--battleship-gray);
+        ">
+      </v-navigation-drawer>
+
+      <v-app-bar app style="background-color: var(--licorice)">
+        <v-toolbar-title style="color: var(--jonquil)">
+          <div style="display: flex; align-items: center">
+            <v-icon class="text-h3">mdi-weight-lifter</v-icon>
+            <p class="text-h4 d-none d-sm-flex" style="margin: 0">DevInGym</p>
+          </div>
+        </v-toolbar-title>
+
+        <v-spacer></v-spacer>
+        <v-btn
+          to="/"
+          text
+          :style="{
+            color: 'var(--jonquil)',
+            'background-color': 'var(--rosewood)',
+            'margin-right': '0.5rem',
+          }">
+          <p class="font-weight-bold d-none d-sm-flex">Home</p>
+          <v-icon class="d-flex d-sm-none">mdi-home-account</v-icon>
+        </v-btn>
+        <v-btn
+          to="/alunos"
+          text
+          :style="{
+            color: 'var(--jonquil)',
+            'background-color': 'var(--rosewood)',
+            'margin-right': '0.5rem',
+          }">
+          <p class="font-weight-bold d-none d-sm-flex">Alunos</p>
+          <v-icon class="d-flex d-sm-none">mdi-account-multiple</v-icon>
+        </v-btn>
+        <v-btn
+          to="/exercicios"
+          text
+          :style="{
+            color: 'var(--jonquil)',
+            'background-color': 'var(--rosewood)',
+            'margin-right': '0.5rem',
+          }">
+          <p class="font-weight-bold d-none d-sm-flex">Exercícios</p>
+          <v-icon class="d-flex d-sm-none">mdi-dumbbell</v-icon>
+        </v-btn>
+        <v-btn
+          to="/login"
+          text
+          :style="{
+            color: 'var(--rosewood)',
+            'background-color': 'var(--jonquil)',
+            'margin-right': '0.5rem',
+          }">
+          <p class="font-weight-bold d-none d-sm-flex">Sair</p>
+          <v-icon class="d-flex d-sm-none">mdi-logout</v-icon>
+        </v-btn>
+      </v-app-bar>
+
+      <v-main
+        style="background-color: var(--lavender-blush); min-height: 100vh">
+        <RouterView />
+      </v-main>
+    </v-app>
   </div>
 </template>
 
@@ -16,16 +76,12 @@
 import { RouterView } from "vue-router";
 </script>
 
-<style scoped>
-header {
-  background-color: #f4f4f4;
-  padding: 1rem;
-  text-align: center;
-}
-
-nav a {
-  margin: 0 1rem;
-  text-decoration: none;
-  color: black;
+<style>
+:root {
+  --jonquil: #f9c80e;
+  --lavender-blush: #f6e8ea;
+  --licorice: #22181c;
+  --rosewood: #5a0001;
+  --battleship-gray: #7e8987;
 }
 </style>
