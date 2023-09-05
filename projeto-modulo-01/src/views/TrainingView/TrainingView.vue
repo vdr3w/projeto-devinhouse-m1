f20b191c707c6f1f10f12349b0ce992d02dc1e88
Drew <84882983+vdr3w@users.noreply.github.com>
2023-09-05 14:34:41 -0300

    feat(ui): Adjust conditional styling for mini card colors

--- projeto-modulo-01/src/views/TrainingView/TrainingView.vue
+++ projeto-modulo-01/src/views/TrainingView/TrainingView.vue
@@ -1,18 +1,37 @@
 <template>
   <v-container fluid :style="{ backgroundColor: 'var(--lavender-blush)' }">
     <v-row>
-      <v-col>
+      <v-col cols="12">
         <v-card :color="'var(--licorice)'">
-          <v-card-title class="headline" :style="{ color: 'var(--jonquil)' }">
-            TREINOS - {{ userName }}
+          <v-card-title
+            class="headline"
+            :style="{
+              color: 'var(--jonquil)',
+              display: 'flex',
+              justifyContent: 'space-between',
+            }">
+            <div class="d-flex align-center">
+              <span>TREINOS - {{ userName }}</span>
+            </div>
+            <div class="d-flex align-center">
+              <router-link to="/alunos">
+                <v-btn
+                  style="
+                    background-color: var(--jonquil);
+                    color: var(--rosewood);
+                  ">
+                  <span class="font-weight-bold">Voltar</span>
+                </v-btn>
+              </router-link>
+            </div>
           </v-card-title>
         </v-card>
       </v-col>
     </v-row>
 
     <v-row>
-      <v-col cols="12" md="6">
-        <v-card max-width="700px" :color="'var(--licorice)'" class="mx-auto">
+      <v-col cols="12">
+        <v-card :color="'var(--licorice)'" class="mx-auto flex-grow-1">
           <v-card-title class="headline" :style="{ color: 'var(--jonquil)' }">
             HOJE
           </v-card-title>
@@ -21,13 +40,24 @@
               <v-col
                 v-for="exercise in todayExercises"
                 :key="exercise.id"
-                cols="12">
-                <v-card class="mb-2">
-                  <v-card-text class="d-flex align-center">
-                    <v-list-item-action>
-                      <v-checkbox :id="exercise.id"></v-checkbox>
+                cols="6">
+                <v-card
+                  @click="toggleCheckbox(exercise)"
+                  class="mb-2 clickable-card">
+                  <v-card-text
+                    class="d-flex align-center"
+                    :style="{
+                      backgroundColor: exercise.checked
+                        ? 'rgba(0, 255, 0, 0.2)'
+                        : 'rgba(255, 0, 0, 0.1)',
+                      opacity: exercise.checked ? 0.5 : 1,
+                    }">
+                    <v-list-item-action class="mt-1 custom-checkbox">
+                      <v-checkbox
+                        :id="exercise.id"
+                        v-model="exercise.checked"></v-checkbox>
                     </v-list-item-action>
-                    <v-list-item-content>
+                    <v-list-item-content class="mb-4">
                       <v-list-item-title>{{ exercise.name }}</v-list-item-title>
                     </v-list-item-content>
                   </v-card-text>
@@ -40,7 +70,7 @@
     </v-row>
 
     <v-row>
-      <v-col>
+      <v-col cols="12">
         <v-card :color="'var(--licorice)'">
           <v-card-actions class="d-flex justify-space-between">
             <v-col
@@ -84,12 +114,19 @@ export default {
     return {
       userName: localStorage.getItem("userName"),
       todayExercises: [
-        { id: 1, name: "Push-up" },
-        { id: 2, name: "Sit-up" },
+        { id: 1, name: "Push-up", checked: false },
+        { id: 2, name: "Sit-up", checked: false },
+        { id: 3, name: "Sit-up", checked: false },
+        { id: 4, name: "Sit-up", checked: false },
+        { id: 5, name: "Sit-up", checked: false },
       ],
       weekExercises: {
         Segunda: [{ id: 1, name: "Push-up" }],
-        Terça: [{ id: 2, name: "Sit-up" }],
+        Terça: [
+          { id: 2, name: "Sit-up" },
+          { id: 3, name: "Sit-up" },
+          { id: 4, name: "Sit-up" },
+        ],
       },
       daysOfWeek: [
         "Segunda",
@@ -103,5 +140,16 @@ export default {
       selectedDay: null,
     };
   },
+  methods: {
+    toggleCheckbox(exercise) {
+      exercise.checked = !exercise.checked;
+    },
+  },
 };
 </script>
+
+<style scoped>
+.clickable-card {
+  cursor: pointer;
+}
+</style>
