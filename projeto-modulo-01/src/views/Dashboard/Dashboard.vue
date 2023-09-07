d75c479c091780be0f4f6d3e3dbe1d8c3839bbf8
Drew <84882983+vdr3w@users.noreply.github.com>
2023-09-06 22:12:00 -0300

    feat(Dashboard) - Changed display position for all items

--- projeto-modulo-01/src/views/Dashboard/Dashboard.vue
+++ projeto-modulo-01/src/views/Dashboard/Dashboard.vue
@@ -1,55 +1,33 @@
 <template>
-  <div style="background-color: #f6e8ea">
-    <v-container fluid>
-      <!-- Centralização vertical para "Bem-vindo" -->
-      <v-row class="fill-height" align="center">
-        <v-col cols="12">
-          <p class="text-center text-h4 text-uppercase">
-            Bem vindo, {{ userName }}
-          </p>
+  <div class="background-color">
+    <v-container fluid class="pa-2 pa-md-4">
+      <v-row
+        class="fill-height pa-1 pa-md-2 d-flex justify-space-between align-center">
+        <v-col cols="auto" class="pa-1 pa-md-2">
+          <p class="headline">Bem vindo, {{ userName }}</p>
         </v-col>
-      </v-row>
-
-      <!-- Centralização vertical para "Imagem" -->
-      <v-row class="fill-height" align="center">
-        <v-col cols="12" class="d-flex align-center justify-center">
-          <v-tooltip text="🥺 Faça carinho na Mel 🐾🐶🧡">
-            <template v-slot:activator="{ props }">
-              <v-img
-                v-bind="props"
-                max-width="400"
-                :width="341"
-                aspect-ratio="16/9"
-                cover
-                src="https://i.imgur.com/GcjBaP9.jpeg"
-                @click="triggerAnimation">
-              </v-img></template
-          ></v-tooltip>
-        </v-col>
-      </v-row>
-      <!-- Centralização vertical para "Hora" -->
-      <v-row class="fill-height" align="center">
-        <v-col cols="12">
-          <p class="text-center text-h4 font-weight-black text-medium-emphasis">
+        <v-col cols="auto" class="pa-1 pa-md-2">
+          <p class="headline font-weight-black">
             Hora Atual: {{ currentTime }}
           </p>
         </v-col>
       </v-row>
 
-      <!-- Centralização vertical e horizontal para os cards -->
-      <v-row class="fill-height" align="center" justify="center">
-        <v-col cols="12" class="d-flex flex-column align-center justify-center">
-          <!-- Card 1 -->
+      <v-row class="fill-height pa-1 pa-md-2 justify-center align-center">
+        <v-col cols="auto" class="pa-1 pa-md-2 mx-auto">
           <v-card
+            max-height="210"
+            min-height="210"
             min-width="300"
             max-width="320"
-            class="square-card elevation-2 mb-4"
+            class="square-card elevation-2"
             style="
               background-color: white;
               color: var(--licorice);
               border: 3px solid var(--licorice);
             ">
             <v-card-title class="border-bottom">
+              <v-icon small left>mdi-account-group</v-icon>
               <h3>Nº de Alunos</h3>
             </v-card-title>
             <v-card-text class="px-4">
@@ -68,9 +46,12 @@
               ></v-card-actions
             >
           </v-card>
+        </v-col>
 
-          <!-- V-Card 2 -->
+        <v-col cols="auto" class="pa-1 pa-md-2 mx-auto">
           <v-card
+            max-height="210"
+            min-height="210"
             min-width="300"
             max-width="320"
             class="square-card elevation-2"
@@ -80,6 +61,7 @@
               border: 3px solid var(--licorice);
             ">
             <v-card-title class="border-bottom">
+              <v-icon small left>mdi-account-group</v-icon>
               <h3>Nº de Exercícios</h3>
             </v-card-title>
             <v-card-text class="px-4">
@@ -100,12 +82,40 @@
           </v-card>
         </v-col>
       </v-row>
+
+      <v-row class="fill-height justify-center align-center">
+        <v-col cols="12" md="12" lg="12">
+          <v-card
+            max-width="400"
+            class="elevation-2 mx-auto"
+            style="background-color: white; text-align: center">
+            <v-tooltip text="🥺 Faça carinho na Mel 🐾🐶🧡">
+              <template v-slot:activator="{ props }">
+                <v-img
+                  max-width="400"
+                  v-bind="props"
+                  class="responsive-image"
+                  src="https://i.imgur.com/GcjBaP9.jpeg"
+                  @click="triggerAnimation"></v-img>
+              </template>
+            </v-tooltip>
+          </v-card>
+        </v-col>
+      </v-row>
+
+      <div
+        v-for="emoji in activeEmojis"
+        :key="emoji.id"
+        :style="emoji.style"
+        class="heart">
+        {{ emoji.text }}
+      </div>
     </v-container>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, reactive, onMounted } from "vue";
 import axios from "axios";
 
 const amount_students = ref(0);
@@ -134,26 +144,47 @@ setInterval(updateTime, 1000);
 
 onMounted(fetchData);
 
-const heartEmojis = ["❤", "💚", "🧡", "💙", "💜", "🤎", "🖤", "🐕", "🐶", "🐾"];
+const emojis = ["❤", "💚", "🧡", "💙", "💜", "🤎", "🖤", "🐕", "🐶", "🐾"];
+
+const activeEmojis = ref([]);
 
 const triggerAnimation = () => {
   for (let i = 0; i < 10; i++) {
-    const heart = document.createElement("div");
-    heart.classList.add("heart");
-    heart.textContent =
-      heartEmojis[Math.floor(Math.random() * heartEmojis.length)];
-    heart.style.setProperty("--n", Math.random());
-    document.body.appendChild(heart);
-
+    const emoji = {
+      id: Date.now() + i,
+      text: emojis[Math.floor(Math.random() * emojis.length)],
+      style: `--n: ${Math.random()};`,
+    };
+    activeEmojis.value.push(emoji);
     setTimeout(() => {
-      document.body.removeChild(heart);
+      const index = activeEmojis.value.indexOf(emoji);
+      if (index > -1) {
+        activeEmojis.value.splice(index, 1);
+      }
     }, 5000);
   }
 };
 </script>
 
 <style>
-/* Seu estilo aqui */
+.background-color {
+  background-color: #f6e8ea;
+}
+
+.responsive-image {
+  max-width: 100%;
+  height: auto;
+}
+
+.headline {
+  font-size: 2rem;
+}
+
+@media (max-width: 600px) {
+  .headline {
+    font-size: 1.5rem;
+  }
+}
 
 .heart {
   font-size: 2em;
@@ -172,12 +203,16 @@ const triggerAnimation = () => {
   }
 }
 
-/* Define your color scheme */
 :root {
   --jonquil: #f9c80e;
   --lavender-blush: #f6e8ea;
   --licorice: #22181c;
   --rosewood: #5a0001;
   --battleship-gray: #7e8987;
+}
+
+.responsive-image {
+  width: 100%;
+  height: auto;
 }
 </style>
